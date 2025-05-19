8bd8ee1057b4d05bf231eceddf409a893b06f7c6
yehia <test@gmail.com>
2025-05-19 19:29:20 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages";
 import About from "./pages/about";
-import SignUp from "./pages/signup";
 import GPACalculator from "./pages/GPACalculator/GPACalculator";
 import CourseCalculator from "./pages/CourseCalculator/CourseCalculator";
-import CalorieCalculator from "./pages/CalorieCalculator/CalorieCalculator";
 
 function App() {
   return (
@@ -17,7 +15,6 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/gpa-calculator" element={<GPACalculator />} />
         <Route path="/course-calculator" element={<CourseCalculator />} />
-        <Route path="/sign-up" element={<CalorieCalculator />} />
       </Routes>
     </Router>
   );
